f6d40c2fbac7c367afbe8bb9f2fbed50c971f0df
Yanting Zeng <zengyt94@g.ucla.edu>
2019-02-10 16:06:01 +0000

    update train.py

--- train.py
+++ train.py
@@ -43,45 +43,49 @@ optimizer = optim.Adam(net.parameters(), lr=0.005)
 
 counter = []
 iteration_number = 0
-total_loss = {"train_loss": [], "val_loss": []} 
+loss_per_epoch = {"train_loss": [], "valid_loss": []}
 num_batch_train = len(train_loader)
 num_batch_val = len(valid_loader)
-print(num_batch_train)
 
 for epoch in range(Config.train_number_epochs):
+    loss_per_batch = {"train_loss": [], "valid_loss": []}
     loss_current_epoch = {"train_loss":[], "val_loss": []}
-    print("Epoch number: %d" % (epoch))
+    print("Epoch number: %d" % (epoch+1))
 
     with tqdm.tqdm(total=num_batch_train) as pbar_train:
         for i, data in enumerate(train_loader, 0): # Train for one epoch
             img0, img1, label = data
             img0, img1, label = img0.cuda(), img1.cuda(), label.cuda()
+            #img0, img1, label = img0, img1, label
             optimizer.zero_grad()
             output1, output2 = net(img0, img1)
             loss_contrastive = criterion(output1, output2, label)
             loss_contrastive.backward()
             optimizer.step()
-            pbar_train.update(1)
             loss = loss_contrastive.item()
-            pbar_train.set_description("train loss: {:.4f}".format(loss))
-            loss_current_epoch["train_loss"].append(loss)
+            loss_per_batch["train_loss"].append(loss)
+            pbar_train.set_description("train loss: {:.4f}".format(np.mean(loss_per_batch['train_loss'])))
+            pbar_train.update(1)
 
     with tqdm.tqdm(total=num_batch_val) as pbar_val:
         for i, data in enumerate(valid_loader, 0): # Evaluation for one epoch
             img0, img1, label = data
             img0, img1, label = img0.cuda(), img1.cuda(), label.cuda()
+            #img0, img1, label = img0, img1, label
             output1, output2 = net.forward(img0, img1)
             loss_contrastive = criterion(output1, output2, label)
             loss = loss_contrastive.item()
-            loss_current_epoch["val_loss"].append(loss)
+            loss_per_batch["valid_loss"].append(loss)
+            pbar_val.set_description("val loss: {:.4f}".format(np.mean(loss_per_batch['valid_loss'])))
             pbar_val.update(1)
-            pbar_val.set_description("val loss: {:.4f}".format(loss))
+    
+    print()
+    for key, value in loss_per_batch.items(): # Collect average loss for current epoch
+        loss_per_epoch[key].append(np.mean(value))
 
-    for key, value in loss_current_epoch.items(): # Collect average loss for current epoch
-        total_loss[key].append(np.mean(value))
 
+torch.save(net.state_dict(), f="/Users/yantiz/Desktop/ML课程/MLP/MLP-Siamese-Network/model/model.pth") # Model saving, Only save the parameters (Recommended)
 
-torch.save(net.state_dict(), f="./model/model.pth") # Model saving, Only save the parameters (Recommended)
-
-# model = model.DeepID().cuda()
-model.load_state_dict(torch.load("./model/model.pth")) # Instantialize the model before loading the parameters
+model = model.DeepID().cuda()
+#model = model.DeepID()
+model.load_state_dict(torch.load("/Users/yantiz/Desktop/ML课程/MLP/MLP-Siamese-Network/model/model.pth")) # Instantialize the model before loading the parameters
