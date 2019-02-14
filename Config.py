61560ce9530966c6467837e956d6c9aefa743571
cxx965165454 <s1807464@ed.ac.uk>
2019-02-14 18:24:52 +0000

    test
    test

--- Config.py
+++ Config.py
@@ -4,6 +4,6 @@ class Config():
     test_dir = "./SampleData/test"
     saved_models_dir = "./saved_models"
     train_number_epochs = 1
-    train_batch_size = 100
-    valid_batch_size = 100
+    train_batch_size = 1000
+    valid_batch_size = 1000
     evaluation_times = 5
