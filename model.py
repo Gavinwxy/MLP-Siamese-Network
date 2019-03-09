68e68ab3c56d3dfd6e06ff08c0061dba4114b5ce
gavinwxy <xiaoyang.wong318@gmail.com>
2019-03-09 09:46:23 +0000

    Update model for triplet loss

--- model.py
+++ model.py
@@ -3,11 +3,12 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class DeepID(nn.Module):
+class DeepID_constrastive(nn.Module):
     input_size = (39, 31)
 
     def __init__(self):
         super(DeepID, self).__init__()
+        self.loss = loss
         self.conv1 = nn.Sequential(
             nn.Conv2d(1, 20, kernel_size=4),
             nn.BatchNorm2d(20),
@@ -44,6 +45,50 @@ class DeepID(nn.Module):
         output1 = self.forward_once(input1)
         output2 = self.forward_once(input2)
         return output1, output2
+
+class DeepID_triplet(nn.Module):
+    input_size = (39, 31)
+
+    def __init__(self):
+        super(DeepID, self).__init__()
+        self.loss = loss
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 20, kernel_size=4),
+            nn.BatchNorm2d(20),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(20, 40, kernel_size=3),
+            nn.BatchNorm2d(40),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(40, 60, kernel_size=3),
+            nn.BatchNorm2d(60),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        # Convolutional Layer 4: Totally unshared
+        self.local2d = nn.Conv2dLocal(in_channels=60, out_channels=80, in_height=3, in_width=2, kernel_size=2, stride=1, padding=0)
+        self.bn = nn.BatchNorm1d(160)
+        self.fc1 = nn.Linear(360, 160)
+        self.fc2 = nn.Linear(160, 160)
+
+    def forward_once(self, x):
+        ### Locally connected layers implemented!
+        out1 = self.conv1(x)
+        out2 = F.relu(self.local2d(out1))
+        out1 = out1.view(out1.shape[0], -1)
+        out2 = out2.view(out2.shape[0], -1)
+        out1 = self.fc1(out1)  ## Fully connected 1
+        out2 = self.fc2(out2)  ## Fully connected 2
+        out = self.bn(torch.add(out1, out2))
+        out = F.relu(out)  # Element-wise sum with ReLU
+        return out
+
+    def forward(self, input1, input2, input3):
+        output1 = self.forward_once(input1)
+        output2 = self.forward_once(input2)
+        output3 = self.forward_once(input3)
+        return output1, output2, output3
 
 
 class ChopraNet(nn.Module):
